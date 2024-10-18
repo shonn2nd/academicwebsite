8cf8560dceef907f497689f360bdc208140066f2
Shonn Cheng <shonn2nd@gmail.com>
2024-10-18 19:50:49 +0800

    f

--- code.R
+++ code.R
@@ -6,10 +6,15 @@ blogdown::build_site()
 #this will help you sucessfully convert rmd to md files
 ---
   title: "test"
+date: "2024-10-18"
 output:
   md_document:
   variant: gfm
-preserve_yaml: true
-date: "2024-10-18"
+  preserve_yaml: true
+authors: 
+  - admin
+tags: [R, dummy variables]
+categories: 
+  - R
 ---
   
